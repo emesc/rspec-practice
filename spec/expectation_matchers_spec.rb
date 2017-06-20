f8aff6b12dfc57367e05ea10c40b3de651b39ff3
emesc <em@curlycoder.me>
2017-06-20 15:37:21 +1000

    Used truthiness matchers

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -47,4 +47,32 @@ describe "Expectation Matchers" do
       end
     end
   end
+
+  describe "truthiness matchers" do
+    it "matches true/false" do
+      expect(1 < 2).to be true
+      expect(1 > 2).to be false
+
+      expect('foo').not_to be true  # the string is not exactly true
+      expect(nil).not_to be false  # nil is not exactly false
+      expect(0).not_to be false   # 0 is not exactly false
+    end
+
+    it "matches truthy/falsey" do
+      expect(1 < 2).to be_truthy
+      expect(1 > 2).to be_falsey
+
+      expect('foo').to be_truthy  # any value counts as true
+      expect(nil).to be_falsey    # nil counts as false
+      expect(0).not_to be_falsey  # but 0 is still not falsey enough
+    end
+
+    it "matches nil" do
+      expect(nil).to be_nil  # either way works
+      expect(nil).to be nil
+
+      expect(false).not_to be_nil   # nil only, just like #nil?
+      expect(0).not_to be_nil       # nil only, just like #nil?
+    end
+  end
 end
