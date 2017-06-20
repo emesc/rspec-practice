243a833406d1910f6d0d005c35bbdb92f533a5c3
emesc <em@curlycoder.me>
2017-06-20 17:05:56 +1000

    Used other useful matchers

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -126,8 +126,52 @@ describe "Expectation Matchers" do
     it "matches strings with a regex" do
       # This matcher is a good way to 'spot check' strings
       string = "The order has been received."
-
       expect(string).to match(/order(.)+received/)
+
+      expect('123').to match(/\d{3}/)
+      expect(123).not_to match(/\d{3}/)
+
+      email = 'someone@somewhere.com'
+      expect(email).to match(/\A\w+@\w+\.\w{3}\Z/)
+    end
+
+    it "matches other object types" do
+      expect('test').to be_instance_of String     # strict, actual value must be an instance/direct descendant of expected value
+      expect('test').to be_an_instance_of String  # alias of #be_instance_of
+
+      expect('test').to be_kind_of String     # loose, expected value can be anywhere in the hierarchy (superclass, module)
+      expect('test').to be_a_kind_of String   # alias, #be_kind_of
+
+      expect('test').to be_a String  # alias #be_kind_of
+      expect([1, 2, 3]).to be_an Array  # alias #be_kind_of
+    end
+
+    it "matches objects with #respond_to" do
+      string = 'test'
+      expect(string).to respond_to :length
+      expect(string).not_to respond_to :sort
+    end
+
+    it "matches class instances with #have_attributes" do
+      class Car
+        attr_accessor :make, :year, :color
+      end
+
+      car = Car.new
+      car.make = 'Dodge'
+      car.year = 2010
+      car.color = 'green'
+
+      expect(car).to have_attributes(color: 'green')
+      expect(car).to have_attributes(make: 'Dodge', year: 2010, color: 'green')
+    end
+
+    it "matches anything with #satisfy" do
+      # this is the most flexible matcher
+      # not used often, use the rest whenever possible
+      expect(10).to satisfy do |value|
+        (value >= 5) && (value <= 10) && (value % 2 == 0)
+      end
     end
   end
 end
