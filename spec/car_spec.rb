a69054c4c5d9d581ddca99e6591747c954c60951
emesc <em@curlycoder.me>
2017-06-21 17:07:59 +1000

    Refactored attributes with a before hook

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,27 +2,27 @@ require 'car'
 
 describe "Car" do
   describe 'attributes' do
+    before(:example) do
+      @car = Car.new
+    end
+
     it "allows reading and writing for :make" do
-      car = Car.new
-      car.make = 'Test'
-      expect(car.make).to eq 'Test'
+      @car.make = 'Test'
+      expect(@car.make).to eq 'Test'
     end
 
     it "allows reading and writing for :year" do
-      car = Car.new
-      car.year = 1999
-      expect(car.year).to eq 1999
+      @car.year = 1999
+      expect(@car.year).to eq 1999
     end
 
     it "allows reading and writing for :color" do
-      car = Car.new
-      car.color = 'foo'
-      expect(car.color).to eq 'foo'
+      @car.color = 'foo'
+      expect(@car.color).to eq 'foo'
     end
 
     it "allows reading for :wheels" do
-      car = Car.new
-      expect(car.wheels).to eq 4
+      expect(@car.wheels).to eq 4
     end
 
     it "allows writing for :doors"
