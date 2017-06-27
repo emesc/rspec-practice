567917d3f6696fa9f13cdf8e9b4b4a56904f6a9f
emesc <em@curlycoder.me>
2017-06-27 14:40:10 +1000

    Added shared examples for standard vehicles

---
+++ spec/shared_examples/a_standard_vehicle.rb
@@ -0,0 +1,24 @@
+shared_examples_for('a standard vehicle') do
+  describe 'attributes' do
+    it "allows reading and writing for :make" do
+      suject.make = 'Test'
+      expect(subject.make).to eq 'Test'
+    end
+
+    it "allows reading and writing for :year" do
+      subject.year = 1999
+      expect(subject.year).to eq 1999
+    end
+
+    it "allows reading and writing for :color" do
+      subject.color = 'foo'
+      expect(subject.color).to eq 'foo'
+    end
+
+    it "allows reading for wheels" do
+      expect(subject.wheels).to eq 4
+    end
+
+    it "allows writing for doors"
+  end
+end
